2b36d996796ffcd51dacc31def0f2f64cbcb376f
Han-jinwook <chiu3@naver.com>
2026-09-24 13:08:51 +0900

    엔진 실행 안내 모달 가이드 현대화 및 기능명세서 최신화

--- ui/app.js
+++ ui/app.js
@@ -3772,7 +3772,7 @@ function downloadSenseBotPackage() {
   a.click();
   document.body.removeChild(a);
 
-  showToast(`📥 [엔진 다운로드] ${ENGINE_ZIP_FILENAME} 다운로드를 시작했습니다. 편한 폴더(바탕화면 등)에 압축을 풀어주세요!`);
+  showToast(`📥 [오프라인 엔진 패키지] ${ENGINE_ZIP_FILENAME} 다운로드를 시작했습니다.`);
 }
 
 /**
